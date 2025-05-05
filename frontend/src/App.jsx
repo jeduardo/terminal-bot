2fc4aabb8c2da492229021501ae6e9eac9d1b467
J. Eduardo <j.eduardo@gmail.com>
2025-05-05 22:17:55 +0200

    Added test for components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import React, { useMemo } from "react";
 
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import { Analytics } from "@vercel/analytics/react";
