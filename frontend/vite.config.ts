b58c178712fa6b01fd8d868527e379f0e03ec1b6
J. Eduardo <j.eduardo@gmail.com>
2025-05-08 14:46:34 +0200

    Added codecov bundler plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { qrcode } from "vite-plugin-qrcode";
+import { codecovVitePlugin } from "@codecov/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [qrcode(), react()],
+  plugins: [
+    qrcode(),
+    react(),
+    codecovVitePlugin({
+      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+      bundleName: "<bundle project name>",
+      uploadToken: process.env.CODECOV_TOKEN,
+    }),
+  ],
   build: {
     outDir: "build",
   },
